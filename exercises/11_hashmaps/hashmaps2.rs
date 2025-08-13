8f64bab9dff48e9b08134765d9040145886d5ef1
NickyHariniaina <nickyharini1na@gmail.com>
2025-08-13 09:47:56 +0300

    add: hashmap3 answer

--- exercises/11_hashmaps/hashmaps2.rs
+++ exercises/11_hashmaps/hashmaps2.rs
@@ -32,6 +32,10 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
+        let is_there = basket.contains_key(&fruit);
+        if !is_there {
+            basket.insert(fruit, 11);
+        }
     }
 }
 
